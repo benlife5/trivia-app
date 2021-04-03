b6a6604bb28e0dea84a34f7cb9570f77c2c6d082
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-04-03 15:32:48 -0400

    checks correct answer and locks question

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
-    .then(res => res.json())
-    .then(res => {
-      console.log(res.results); 
-      res.results.map(question => question.answered=false);
-      setQuestions(res.results);
-    })
-  }, [])
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setQuestions(res.results);
+      });
+  }, []);
 
   return (
     <div className="App">
--- src/Question.js
+++ src/Question.js
@@ -2,6 +2,8 @@ import {useState} from "react";
 
 function Question(props) {
   const question = props.question;
+  const [disabled, setDisabled] = useState(false);
+  const [displayResult, setDisplayResult] = useState(<></>);
 
   let randomLocation;
   if (question.type === "multiple") {
@@ -13,12 +15,23 @@ function Question(props) {
   let potentialAnswers = [...question.incorrect_answers];
   potentialAnswers.splice(randomLocation, 0, question.correct_answer);
 
+
+  const answerSelected = (answer) => {
+    setDisabled(true);
+    if (answer === question.correct_answer) {
+        setDisplayResult(<p>Correct! The answer was {question.correct_answer}.</p>);
+    } else {
+        setDisplayResult(<p>Incorrect. The answer was {question.correct_answer}.</p>);
+    }
+  }
+
+
+
   return (
     <>
       <h4>{question.question}</h4>
-      {
-        potentialAnswers.map(answer => <input type="submit" value={answer} key={answer} />)
-      }
+      {potentialAnswers.map(answer => <input type="submit" disabled={disabled} value={answer} key={answer} onClick={() => answerSelected(answer)}/>)}
+      {displayResult}
     </>
   );
 }
