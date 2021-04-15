d2dc75ee169999cf088ef72c616a499ee4b49898
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-04-14 23:46:18 -0400

    added number formatting

--- src/Score.js
+++ src/Score.js
@@ -3,13 +3,25 @@ function Score (props) {
   const correct = props.correct;
   const incorrect = props.incorrect;
 
-  return (
-    <div>
-      <p>Unanswered: {unanswered}</p>
-      <p>Correct: {correct / (correct + incorrect) * 100}% ({correct})</p>
-      <p>Incorrect: {incorrect / (correct + incorrect) * 100}% ({incorrect})</p>
-    </div>
-  );
+  const percentageCorrect = correct / (correct + incorrect);
+  const percentageIncorrect = 1 - percentageCorrect;
+
+  const formattingOptions = {
+    style: "percent",
+    maximumFractionDigits: 2
+  }
+
+  const formatter = new Intl.NumberFormat(undefined, formattingOptions);
+  
+  if (correct + incorrect !== 0) {
+    return (
+      <div>
+        <p>Correct: {formatter.format(percentageCorrect)} ({correct})</p>
+        <p>Incorrect: {formatter.format(percentageIncorrect)} ({incorrect})</p>
+      </div>
+    );
+  }
+  return null;
 }
 
 export default Score;
