aec2c87c98155ac91d3d8efa0de9cad376a1fe40
avb5bt <81479081+avb5bt@users.noreply.github.com>
2022-05-23 16:30:51 -0400

    refactored weather content

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,12 +1,23 @@
+import './App.css';
+import Controls from "./Controls.js";
+import Typography from '@material-ui/core/Typography';
+import CurrentWeather from "./CurrentWeather.jsx";
 
-
-function WeatherApp() {
+function WeatherApp(props) {
   
 
   
-
+    
   return (
-    <></>
+    <div className="weatherPart">
+          <div className="sidebar">
+            <Typography variant="h2" className="title"> WEATHER </Typography>
+            <Controls  data={props.latData} generateArticles={props.generateArticles} generateIcon={props.generateIcon} generateWeather={props.generateWeather} generateLatitudeAndLongitude={props.generateLatitudeAndLongitude} />
+          </div>
+          <div className="weatherCard">
+            <CurrentWeather  articles={props.articles} icon={props.icon} data={props.weatherData} generateArticles={props.generateArticles} generateWeather={props.generateWeather}/>
+          </div>
+    </div>
   );
 }
 
